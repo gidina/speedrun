6b30d3738511ec98781d9532ca9bf8b720e6aa92
Georgina Serra Oliveras <Gidina@MacBook-MacBook-Pro-de-Georgina.local>
2019-01-26 18:55:58 +0100

    Fetching and rendering games

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
+import fetch from 'isomorphic-unfetch';
 import "../styles.css";
 
 const Home = (props) => (
-    <div className="test-css">Home</div>
+    <div>
+        <h1 className="example">Games</h1>
+        <ul>
+            {props.games.map((game) => (
+                <li key={game.id}>
+                    <img alt="" src={game.assets['cover-small'].uri} />
+                    {game.names.international}
+                </li>
+            ))}
+        </ul>
+    </div>
 );
+
+Home.getInitialProps = async () => {
+    const res = await fetch('https://www.speedrun.com/api/v1/games');
+    const result = await res.json();
+
+    return {
+        games: result.data
+    };
+};
 
 export default Home;
