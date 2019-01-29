1b69f37fc73aa4be690128c1197d0eeeaa896099
Georgina Serra Oliveras <Gidina@MacBook-MacBook-Pro-de-Georgina.local>
2019-01-29 22:28:32 +0100

    Change Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/']
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+    moduleNameMapper:{
+        "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+        "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js"
+    }
 }  
