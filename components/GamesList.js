f9c74453e2657f29152b4ba8553a3988e45ae91e
Georgina Serra Oliveras <Gidina@MacBook-MacBook-Pro-de-Georgina.local>
2019-01-31 17:50:02 +0100

    Change Loading for GamesList component

--- components/GamesList.js
+++ components/GamesList.js
@@ -5,9 +5,8 @@ import PropTypes from 'prop-types';
 import "./index.css";
 
 const GamesList = props => {
-    console.log(props.isFetchingGames);
     if (props.isFetchingGames) {
-        return "LOadingggg.........";
+        return <div>Loading...</div>;
     }
     return (
         <div>
@@ -25,22 +24,6 @@ const GamesList = props => {
         </div>
     );
 };
-
-// const GamesList = props => (
-//     <div>
-//         <h1 className="games-title">Games</h1>
-//         <ul className="games-list">
-//             {props.games.map((game) => (
-//                 <li key={game.id} className="game">
-//                     <img alt="" className="game-img" src={game.assets['cover-small'].uri} />
-//                     <Link as={`/game/${game.id}`} href={`/game?id=${game.id}`}>
-//                         <a className="game-title">{game.names.international}</a>
-//                     </Link>
-//                 </li>
-//             ))}
-//         </ul>
-//     </div>
-// );
 
 export default GamesList;
 
