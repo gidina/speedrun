d7baf670bc472c5c3e5449af0fb11bf1d9937d1c
Georgina Serra Oliveras <Gidina@MacBook-MacBook-Pro-de-Georgina.local>
2019-02-01 17:59:26 +0100

    Refactor components styles

--- components/Game.js
+++ components/Game.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 
-import "./index.css";
+import "./Game.css";
 
 const Game = props => {
     const { game } = props;
--- components/GameDetail.js
+++ components/GameDetail.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-import "./game.css";
+import "./GameDetail.css";
 
 const GameDetail = props => {
     const { isLoadingRuns, selectedGame } = props;
